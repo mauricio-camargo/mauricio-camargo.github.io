ca379c0ae3ed3e8f55504af9693cdd58157e32ac
mauricio-camargo <camargofurg@gmail.com>
2024-08-07 15:07:36 -0300

    asd

---
+++ r-eutilizaveis/Config de Tokens.R
@@ -0,0 +1,10 @@
+#Não pode salvar o Token aqui, senão Github 
+#não deixa fazer Push.
+
+usethis::use_git_config(
+  user.name = "mauricio-camargo",
+  user.email = "camargofurg@gmail.com")
+
+usethis::create_github_token()
+
+gitcreds::gitcreds_set()
